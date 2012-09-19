20714f689475d91b12db6e9e98aa95666a305c52
oddcb <oddchrister@gmail.com>
2012-09-19 18:05:27 +0200

    Flyttet kode fra main.rb inn i utgiftsrapport.rb. JSON-sakene etc

--- utgiftsrapport.rb
+++ utgiftsrapport.rb
@@ -1,5 +1,15 @@
 class Utgiftsrapport < Sinatra::Base
   get "/" do
-    "Hello WOrldz! "
+    "Hello WOrldz!"
   end
+
+  configure do
+    mime_type :json, 'application/json'
+  end
+
+  get '/service' do
+    content_type :json
+    "{name: 'Utgiftsrapporteringssystem Enterprise Edition', version: 0.1}"
+  end
+
 end
