c223dff90c3fc9cc93c0a50806441d6d17ed340a
Bendik Solheim <bendiksolheim@gmail.com>
2012-09-19 19:25:16 +0200

    La på litt styling

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,5 +1,11 @@
 function UtgiftCtrl($scope) {
-  $scope.utgifter = [];
+  $scope.utgifter = [
+    {tittel: "Hola", sum: "120948"},
+    {tittel: "Hola2 awfd", sum: "749234"},
+    {tittel: "Hola3 woeigj iigwh ", sum: "3"},
+    {tittel: "Hola4777q kjsefoqkw", sum: "2380891"},
+    {tittel: "Hola5", sum: "123"}
+  ];
  
   $scope.addUtgift = function() {
     var sum = parseFloat($scope.sum, 10);
