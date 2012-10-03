eaaa8efa5a4b95ea78b30bf366c7cedb5ad98731
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-10-03 19:34:25 +0200

    Lagt til funksjonalitet for å lese og skrive til mongodb

--- utgiftsrapport.rb
+++ utgiftsrapport.rb
@@ -1,4 +1,7 @@
 class Utgiftsrapport < Sinatra::Base
+  configure :development do 
+    register Sinatra::Reloader
+  end
   get "/" do
     "Hello WOrldz!"
   end
@@ -12,4 +15,26 @@ class Utgiftsrapport < Sinatra::Base
     "{name: 'Utgiftsrapporteringssystem Enterprise Edition', version: 0.1}"
   end
 
+  post '/insert' do
+    @conn = Mongo::Connection.new
+    @db = @conn['test']
+    @coll = @db['usysdev']
+    # TODO: Legg hent user_id fra egnet sted
+    if params[:id] == ""
+      inserted = @coll.insert({'user_id' => 1, 'data' => params[:data].to_s })
+    else
+      inserted = @coll.update({'_id' => BSON::ObjectId(params[:id])}, {'user_id' => 1, 'data' => params[:data].to_s})
+    end
+    content_type :json
+    inserted.to_s
+  end
+
+  get '/get_usys_data' do
+    @conn = Mongo::Connection.new
+    @db = @conn['test']
+    @coll = @db['usysdev']
+    inserted = @coll.find({'user_id' => 1})
+    content_type :json
+    inserted.first.to_s
+  end
 end
