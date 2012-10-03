5099eab619d5e0db66c5621db19045c6a3b42406
Jørgen Jervidalo <jervidalo@gmail.com>
2012-10-03 17:56:48 +0200

    Laget single page app med to sider og templating

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,3 +16,6 @@ function UtgiftCtrl($scope) {
     $('#title-input').focus();
   };
 }
+
+function RenderReportCtrl($scope) {
+}
---
+++ public/js/usys.js
@@ -0,0 +1,6 @@
+angular.module('usys', []).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/renderReport', {templateUrl: 'tpl/renderReport.html', controller: RenderReportCtrl}).
+      otherwise({templateUrl: 'tpl/addReceipt.html', controller: UtgiftCtrl});
+  }]);
