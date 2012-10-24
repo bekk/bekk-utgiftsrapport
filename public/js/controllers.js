83ea90e9041a578a313ec3d03ae6c1a49e6ba887
Holger Ludvigsen <ludvigsen@gmail.com>
2012-10-24 18:21:54 +0200

    Henter nå utgifter fra databasen også når man går rett på /renderReport

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,18 +1,18 @@
-function UtgiftCtrl($scope, $http, sharedProperties) {
-  function refreshList() {
-    jQuery.ajax({url: "/utgifter", 
-      success: function(data) {
-        sharedProperties.setUtgifter(data);
-        $scope.utgifter = data
+function refreshList($scope, sharedProperties) {
+  jQuery.ajax({url: "/utgifter", 
+    success: function(data) {
+      sharedProperties.setUtgifter(data);
+      $scope.utgifter = data
+    },
+    error: function(data, status, headers, config) {
+        alert("error");
       },
-      error: function(data, status, headers, config) {
-          alert("error");
-        },
-      async: false
-    });
-  }
+    async: false
+  });
+}
 
-  refreshList();
+function UtgiftCtrl($scope, $http, sharedProperties) {
+  refreshList($scope, sharedProperties);
 
   $scope.utgifter = sharedProperties.getUtgifter();
  
@@ -24,7 +24,7 @@ function UtgiftCtrl($scope, $http, sharedProperties) {
       type: "POST",
       data: {tittel: $scope.tittel, sum: sum},
       success: function(data) {
-          refreshList();
+          refreshList($scope, sharedProperties);
         },
       error: function(data, status, headers, config) {
           alert("error");
@@ -45,7 +45,7 @@ function UtgiftCtrl($scope, $http, sharedProperties) {
       type: "DELETE",
       data: {"id": id},
       success: function(data, status, headers, config) {
-          refreshList();
+          refreshList($scope, sharedProperties);
         },
       error: function(data, status, headers, config) {
           alert("error");
@@ -56,6 +56,7 @@ function UtgiftCtrl($scope, $http, sharedProperties) {
 }
 
 function RenderReportCtrl($scope, sharedProperties) {
+  refreshList($scope, sharedProperties);
   
 	$scope.utgifter = sharedProperties.getUtgifter();
 	var matrix = utils.receiptsToMatrix($scope.utgifter);
