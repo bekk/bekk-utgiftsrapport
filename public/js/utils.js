663854766e2cc5ede8b345b9749861cc62c7f0d8
Holger Ludvigsen <ludvigsen@gmail.com>
2012-10-24 18:26:10 +0200

    Pynter litt på rapporten

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,13 +3,14 @@ var utils = (function() {
 		var matrix = [];
 
 		for (var i in receipts) {
-			var row = [receipts[i].tittel];
+			var row = [];
 
-			for (var j in receipts) {
+			for (var j = 0; j < receipts.length*2; j++) {
 				row.push(null);
 			}
 
-			row[parseInt(i)+1] = receipts[i].sum;
+			row[parseInt(i,10)*2] = parseInt(i,10)+1;
+			row[parseInt(i,10)*2+1] = receipts[i].sum;
 
 			matrix.push(row);
 		}
