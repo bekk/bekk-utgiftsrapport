45f23624d42d78cd1cfb57be59b4e7447acc1bc1
Holger Ludvigsen <ludvigsen@gmail.com>
2012-10-24 21:47:07 +0200

    Fikser en merge som gikk dårlig pga forskjellig intendation-width

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,67 +1,83 @@
 function refreshList($scope, sharedProperties) {
-  jQuery.ajax({url: "/utgifter", 
-    success: function(data) {
-      sharedProperties.setUtgifter(data);
-      $scope.utgifter = data
-    },
-    error: function(data, status, headers, config) {
-        alert("error");
-      },
-    async: false
-  });
+    jQuery.ajax({url: "/utgifter", 
+        success: function(data) {
+            sharedProperties.setUtgifter(data);
+            $scope.utgifter = data
+        },
+        error: function(data, status, headers, config) {
+                alert("error");
+            },
+        async: false
+    });
 }
 
 function UtgiftCtrl($scope, $http, sharedProperties) {
-  refreshList($scope, sharedProperties);
+    refreshList($scope, sharedProperties);
 
-  $scope.utgifter = sharedProperties.getUtgifter();
+    $scope.utgifter = sharedProperties.getUtgifter();
  
-  $scope.addUtgift = function() {
-    var sum = parseFloat($scope.sum, 10);
+    $scope.addUtgift = function() {
+        var sum = parseFloat($scope.sum, 10);
 
-    jQuery.ajax({
-      url: "/utgift", 
-      type: "POST",
-      data: {tittel: $scope.tittel, sum: sum},
-      success: function(data) {
-          refreshList($scope, sharedProperties);
-        },
-      error: function(data, status, headers, config) {
-          alert("error");
-        },
-      async: false
-    });
+        jQuery.ajax({
+            url: "/utgift", 
+            type: "POST",
+            data: {tittel: $scope.tittel, sum: sum},
+            success: function(data) {
+                    refreshList($scope, sharedProperties);
+                },
+            error: function(data, status, headers, config) {
+                    alert("error");
+                },
+            async: false
+        });
 
-    $scope.tittel = null;
-    $scope.sum = null;
+        $scope.tittel = null;
+        $scope.sum = null;
 
-    $('#title-input').focus();
-  };
+        $('#title-input').focus();
+    };
 
-  $scope.slettUtgift = function(event) {
-    var id = event.srcElement.id;
-    jQuery.ajax({
-      url: "/utgift",
-      type: "DELETE",
-      data: {"id": id},
-      success: function(data, status, headers, config) {
-          refreshList($scope, sharedProperties);
-        },
-      error: function(data, status, headers, config) {
-          alert("error");
-        },
-      async: false
-    });
-  };
+    $scope.slettUtgift = function(event) {
+        var id = event.srcElement.id;
+        jQuery.ajax({
+            url: "/utgift",
+            type: "DELETE",
+            data: {"id": id},
+            success: function(data, status, headers, config) {
+                    refreshList($scope, sharedProperties);
+                },
+            error: function(data, status, headers, config) {
+                    alert("error");
+                },
+            async: false
+        });
+    };
 }
 
 function RenderReportCtrl($scope, sharedProperties) {
-  refreshList($scope, sharedProperties);
+    refreshList($scope, sharedProperties);
 
 	$scope.utgifter = sharedProperties.getUtgifter();
 	var matrix = utils.receiptsToMatrix($scope.utgifter);
 
 	$scope.matrix = matrix;
 
-  $scope.$on('$viewContentLoaded', doRepeat);
+    $scope.$on('$viewContentLoaded', doRepeat);
+
+    $scope.genererRapport = function (event) {
+        jQuery.ajax({
+            url:"/rapport",
+            type:"POST",
+            success:function (data, status, headers, config) {
+                // redirect til rapport?
+            },
+
+            error:function (data, status, headers, config) {
+                // push en feilmelding til toppen av siden man er på?
+                alert("noe gikk galt ved generering.")
+            },
+            async:false
+        })
+    }
 }
